4dc0d51db8c11a15b57fb987f6a90cc47206e68d
Shriverdhan Pathak <118191820+shrixx18@users.noreply.github.com>
2026-04-18 07:53:02 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        for(int i = 0; i<n; i++){
+            mp[nums[i]]++;
+        }
+        for(auto &pair:mp){
+            if(pair.second>1) return true;
+        }
+        return false;
+    }
+};
